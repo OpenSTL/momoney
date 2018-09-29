1498d133ea3f5e1288aa49f5a472ca8419f708cd
Mike Win <mikeIsAnAsset@gmail.com>
2018-09-29 13:49:38 -0500

    Initial commit

---
+++ src/main/java/com/municipalityBudgetsSTL/MunicipalityBudgetsStlApplication.java
@@ -0,0 +1,12 @@
+package com.municipalityBudgetsSTL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunicipalityBudgetsStlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunicipalityBudgetsStlApplication.class, args);
+	}
+}
