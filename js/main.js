23461ca642f76ae4416a59ec624a290b8d429c05
Tom Kent <tom@teeks99.com>
2018-11-03 10:43:00 -0500

    Fixed URL to point to new data location

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 */
 
 // CSV needs to be http/https (not file://) to load
-var tempUrl = "https://raw.githubusercontent.com/OpenDataSTL/municipalityBudgetsSTL/clean_numbers/muniBudgetV1/data/budget.csv"
+var tempUrl = "https://raw.githubusercontent.com/OpenDataSTL/municipalityBudgetsSTL/master/data/budget.csv"
 
 d3.csv(tempUrl).then(function(rows){
     rows.forEach(function(row){
