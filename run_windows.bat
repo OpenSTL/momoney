be913f76ecba0d59a59c8389268ae15c0f97efa8
Tom Kent <tom@teeks99.com>
2018-11-18 15:50:41 -0600

    Bug in existing windows script.

--- run_windows.bat
+++ run_windows.bat
@@ -2,11 +2,11 @@ set OUTPUTDIR=output
 set PELICANCONF=pelicanconf.py
 
 call make_venv.bat
-call .venv/Scripts/activate.bat
+call .venv\Scripts\activate.bat
 
 pip install -r requirements.txt
 
 pelican content -o %OUTPUTDIR% -s %PELICANCONF%
 
 cd %OUTPUTDIR%
-python -m pelican.server
+pelican -l
