3a11be9de50fa8aa81cbf34f42e51dffa5824127
Tom Kent <tom@teeks99.com>
2019-01-10 06:44:35 -0600

    Adding full data from the spreadsheet as single file in both csv and json format.

--- data/export_data.py
+++ data/export_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import json
 
 workbook_filename = "import_data.xlsx"
 out_json = "../content/data/full.json"
@@ -18,12 +19,29 @@ def combine(data):
         data["Debts"]], axis=1)
 
 def filter(data):
-    valid_rows =(data != "Information Not Provided").all(1) 
+    valid_rows = (data != "Information Not Provided").all(1) 
     filtered_data = data[valid_rows]
+
+    valid_rows = (filtered_data != "No Breakdown Provided").all(1)
+    filtered_data = filtered_data[valid_rows]
+
+    valid_rows = (filtered_data != "Breakdown of taxes not provided").all(1)
+    filtered_data = filtered_data[valid_rows]
+
+    # Duplicate columns
+    filtered_data = filtered_data.loc[:,~filtered_data.columns.duplicated()]
+
     return filtered_data
 
 def make_dict(data):
-    pass
+    return data.to_dict(orient='index')
+
+def write_json(data, file):
+    with open(file, 'w') as f:
+        if isinstance(data, pd.DataFrame):
+            data.to_json(f, orient="index")
+        else:
+            json.dump(data, f, indent=2)
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -32,4 +50,8 @@ if __name__ == "__main__":
     combined_data = combine(data)
     filtered_data = filter(combined_data)
     filtered_data.to_csv(out_csv)
-    print(combined_data)
+    #write_json(filtered_data, out_json)
+    dict_data = make_dict(filtered_data)
+    write_json(dict_data, out_json)
+
+    print("Data processing complete")
