2e9bcc7babbc565088f2bf47a2dda428d1ad0669
ssharieff <saadsharieff@gmail.com>
2019-02-11 18:54:30 -0600

    brought everything from bars into master.

--- content/js/main.js
+++ content/js/main.js
@@ -4,8 +4,18 @@
 *    2.7 - Loading external data
 */
 
-function filterOutInvalidRows(rows)
-{
+function largest(rows, field){
+    var highest = 0;
+    rows.forEach(function(row){
+        if (row[field] > highest){
+                highest = row[field];
+        }
+    });
+    return highest;
+};
+
+
+function filterOutInvalidRows(rows) {
     var filtered_rows = rows.filter(function(row){
         if (isNaN(row["BUDGET YEAR"])){ return false; }
         if (isNaN(row["TOTAL REVENUE"])){ return false; }
@@ -24,6 +34,11 @@ function averageRevenue(rows){
     return totalRevenueAverage;
 }
 
+var barWidth = 25;
+var barSpacing = 25;
+
+
+// Pull in D3 CSV data
 d3.csv("../data/full.csv").then(function(rows){
     rows.forEach(function(row){
         // Convert strings to integers
@@ -38,45 +53,52 @@ d3.csv("../data/full.csv").then(function(rows){
 
     var totalRevenueAverage = averageRevenue(filtered_rows);
 
+    var areaHeight = rows.length * (barWidth + barSpacing);
+    var areaWidth = 1400;
+    
+        
+    // scaling the data to the svg area
+    var y = d3.scaleLinear()
+    .domain([0,largest(rows,"POPULATION")]) // largest population
+    .range([0,areaWidth]);
+
+    // set SVG height an width
     var svg = d3.select("#chart-area").append("svg")
-        .attr("width", 1400)
-        .attr("height", 1400);
-
-    var circles = svg.selectAll("circle")
-        .data(filtered_rows);
-
-    circles.enter()
-        .append("circle")
-            .attr("cx", function(d, i){
-                console.log(d);
-                return ((i * 50) + 25);
+        .attr("width", areaWidth)
+        .attr("height", areaHeight);
+    
+    // brings in municipality name and displays it on the screen
+    var text = svg.selectAll("text")
+        .data(rows);
+    text.enter()
+        .append("text")
+            .attr("x", 10)
+            .attr("y",function(d,i){
+                return (i * (barWidth + barSpacing)) + 30;
             })
-            .attr("cy", function(d, i){
-                return ((i * 50) + 25);
+            .text(function(d){
+                return d.MUNICIPALITY;
             })
-            .attr("r", function(d){
-                return d["TOTAL REVENUE"]/totalRevenueAverage * 25;
+            .attr("font-size",21)
+            .attr("fill","orange")
+            .attr("class","text");
+    
+    console.log("this works");
+    
+    // brings in population data and displays it as horizontal bars
+    var rect = svg.selectAll("rect")
+        .data(rows);
+    rect.enter()
+        .append("rect")
+            .attr("x", 10)
+            .attr("y",function(d,i){
+                return (i * (barWidth + barSpacing)) + 35;
             })
-            .attr("stroke","black")
-	          .attr("fill", "white")
-
-    var text = svg.selectAll('text')
-        .data(filtered_rows)
-        .enter()
-        .append('text')
-          .attr("x", function(d, i){
-              return (i * 50) + 25;
-          })
-          .attr("y", function(d, i){
-              return ((i * 50) + 25);
-          })
-          .text( function (d) {
-                     return d.MUNICIPALITY;
-                   })
-                   .attr("font-family", "sans-serif")
-                   .attr("font-size", "10px")
-                   .attr("fill", "red")
-                   .attr("text-anchor", "middle")
+            .attr("width",function(d){
+                return y(d.POPULATION);
+            })
+            .attr("height",barWidth)
+            .attr("fill","grey");
 
 }).catch(function(error){
     console.log(error);
