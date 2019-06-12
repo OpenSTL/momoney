44b5bab9252718d7ffa5d4962cfa2a8c61937886
bransonf <32143148+bransonf@users.noreply.github.com>
2019-06-12 11:04:57 -0500

    add mo_map contents

---
+++ mo_map/js/map.js
@@ -0,0 +1,21 @@
+var map = L.map('map').setView([38.6570, -90.4511], 10);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(map);
+
+
+// Adding GeoJSON formatted Data
+L.geoJSON(boundary).addTo(map);
+
+
+
+
+
+
+
+
+
+
